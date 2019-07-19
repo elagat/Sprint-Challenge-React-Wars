38fd82d896183c75d13e3c392ebce396c4eb9ee2
Emily Lagattolla <elagattolla@gmail.com>
2019-07-19 13:20:38 -0500

    used semantic-ui for styling of cards

--- starwars/src/index.js
+++ starwars/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 import './index.css';
 import App from './App';
 
